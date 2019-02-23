89eaadc7b8a148d2d2e2e7fefe22e10800e60e8b
Gregory Theos <gdtheos@lg-1r17-n03.guillimin.clumeq.ca>
2019-02-23 14:10:21 -0500

    Small bug fixes in R scripts

--- smallModels/FG.R
+++ smallModels/FG.R
@@ -1,9 +1,10 @@
 library(brms)
 library(loo)
 morphoData <- read.csv(file="ELPMorphoParsed.csv", header=TRUE, sep=",")
+trainingData <- morphoData[sample(nrow(morphoData), 10000`),]
 model <- brm(formula = log2(reactionTime) ~ trialNum + gender + education + suffixType 
-		+ orthoLength + OLD20 + syllables + (10^wholeFreq*log2(10^wholeFreq))/(10^wholeFreq + 10^baseFreq)+(10^baseFreq*log2(10^baseFreq)/(10^wholeFreq + 10^baseFreq) + (1|subject),
-            data = sample_n(morphoData, 10000), family = gaussian(),
+		+ orthoLength + OLD20 + syllables + (10^wholeFreq*log2(10^wholeFreq))/(10^wholeFreq + 10^baseFreq)+(10^baseFreq*log2(10^baseFreq))/(10^wholeFreq + 10^baseFreq) + (1|subject),
+            data = trainingData, family = gaussian(),
             prior = set_prior("normal(0,5)", class = "b"),
             warmup = 200, iter = 1000, chains = 4, cores=4)
 saveRDS(model, file="FG.RDS")
--- smallModels/fullStorage.R
+++ smallModels/fullStorage.R
@@ -1,9 +1,10 @@
 library(brms)
 library(loo)
 morphoData <- read.csv(file="ELPMorphoParsed.csv", header=TRUE, sep=",")
+trainingData <- morphoData[sample(nrow(morphoData), 10000),]
 model <- brm(formula = log2(reactionTime) ~ trialNum + gender + education + suffixType 
 		+ orthoLength + OLD20 + syllables + wholeFreq + (1|subject),
-            data = sample_n(morphoData, 10000), family = gaussian(),
+            data = trainingData, family = gaussian(),
             prior = set_prior("normal(0,5)", class = "b"),
             warmup = 200, iter = 1000, chains = 4, cores=4)
 saveRDS(model, file="fullStorage.RDS")
